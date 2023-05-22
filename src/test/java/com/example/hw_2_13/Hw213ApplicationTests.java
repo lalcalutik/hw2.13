9e686a691d55c1f15b3914079b623454886caec7
ПК <themorozushka@gmail.com>
2023-05-22 13:54:26 +0300

    initial commit

---
+++ src/test/java/com/example/hw_2_13/Hw213ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hw_2_13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw213ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
